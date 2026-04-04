f3c8e7040aab0fa84ae945cbf230df9c8a7c188b
Harshit saxena <saxena24harshit@gmail.com>
2026-04-04 19:58:01 +0530

    fixed memory leak issue

--- client/app/(HomeComponents)/blogList.tsx
+++ client/app/(HomeComponents)/blogList.tsx
@@ -1,67 +1,51 @@
 "use client"
-import { motion, useMotionValue } from "motion/react"
-import { useRef, useState } from "react";
-  const cardVariant = [
-    "bg-(--accent-peach) text-[color-mix(in_srgb,var(--accent-peach),black_20%)] shadow-[-10px_0_color-mix(in_srgb,var(--accent-peach),black_10%)] drop-shadow-lg",
-    "bg-(--accent-orange) text-[color-mix(in_srgb,var(--accent-orange),black_20%)] shadow-[-10px_0_color-mix(in_srgb,var(--accent-orange),black_10%)] drop-shadow-lg",
-    "bg-(--accent-yellow) text-[color-mix(in_srgb,var(--accent-yellow),black_20%)] shadow-[-10px_0_color-mix(in_srgb,var(--accent-yellow),black_10%)] drop-shadow-lg",
-  ];
-  const blog = [
-    {
-      title: "Understanding React Server Components",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-    },
-    {
-      title: "The Power of Tailwind CSS",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-    },
-    {
-      title: "Mastering TypeScript",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-    },
-    {
-      title: "Building Accessible Web Apps",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-    },
-    {
-      title: "Next.js App Router Guide",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-    },
-  ];
+import { motion, useMotionValue, useTransform } from "motion/react"
+import { useRef } from "react"
 
-type Event = MouseEvent | TouchEvent | PointerEvent
+const cardVariant = [
+  "bg-(--accent-peach)",
+  "bg-(--accent-orange)",
+  "bg-(--accent-yellow)",
+]
+
+const blog = [
+  { title: "Understanding React Server Components", description: "..." },
+  { title: "The Power of Tailwind CSS", description: "..." },
+  { title: "Mastering TypeScript", description: "..." },
+  { title: "Building Accessible Web Apps", description: "..." },
+  { title: "Next.js App Router Guide", description: "..." },
+]
+
+function DraggableCard({item, i, dradConstraintRef }:{item:any, i:number, dradConstraintRef:any}) {
+  return (
+    <motion.div
+      id={i.toString()}
+      drag="x"
+      dragConstraints={dradConstraintRef}
+      onDragEnd={(event, info) => {
+        console.log(info)
+        if (info.offset.x > 100) {
+          const currentItem = document.getElementById(i.toString())
+          console.log("Right swipe", currentItem?.classList.add("opacity-50"))
+        } else if (info.offset.x < -100) {
+          console.log("Left swipe", i)
+        }
+      }}
+      className={`flex flex-col gap-2 p-8 my-6 rounded-2xl ${cardVariant[i % cardVariant.length]}`}
+    >
+      <span className="font-bold">{item.title}</span>
+      <h1>{item.description}</h1>
+    </motion.div>
+  )
+}
 
 export default function BlogList() {
-
-  const dragConstraintsRef = useRef(null)
-  const [draggedItem, setDraggedItem] = useState<boolean | null>(false)
-
-  function dragEnd(i:number,x:any) {
-    let currentDragElement = document.getElementById(i.toString())
-    let elementPosition = x.get()
-    if(elementPosition < -50){
-      console.log("left side drag" ,x)
-      currentDragElement?.classList.add("bg-green-400")
-    }else if(elementPosition > 50  ) {
-      console.log("right side drag", elementPosition)
-      currentDragElement?.classList.add("opacity-25")
-    }
-    setDraggedItem(true)
-  }
+  const dradConstraintRef = useRef(null)
   return (
-    <div ref={dragConstraintsRef}>
-      {blog.map((item, i) => {
-        const x = useMotionValue(0);
-        return (
-          <motion.div id={i.toString()} drag="x" style={{x}} dragMomentum={false} dragConstraints={dragConstraintsRef} onDragEnd={()=>dragEnd(i,x)} draggable={false}
-            key={i} className={`flex flex-col gap-2 p-8 my-6 rounded-2xl ${cardVariant[i % cardVariant.length]}`}>
-            <span className="font-bold"> {item.title} </span>
-            <h1 className="text-(--text-primary)">{item.description}</h1>
-          </motion.div>
-
-        )
-      })}
-
+    <div ref={dradConstraintRef}>
+      {blog.map((item, i) => (
+        <DraggableCard key={i} item={item} i={i} dradConstraintRef={dradConstraintRef} />
+      ))}
     </div>
-  );
+  )
 }
---
+++ client/app/login/page.tsx
@@ -0,0 +1,8 @@
+
+export const Login = () => {
+  return (
+    <div>
+        <h1>this is login pagecd</h1>
+    </div>
+  )
+}
