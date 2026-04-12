88bc136fee6486c57b7efd5413833fd2585675ce
Harshit saxena <saxena24harshit@gmail.com>
2026-04-12 21:28:48 +0530

    add task form styling update

--- client/app/(HomeComponents)/addTask.tsx
+++ client/app/(HomeComponents)/addTask.tsx
@@ -9,15 +9,15 @@ function addTask() {
   return (
   <>
     {addFormToggle && <InputForm />}
-    <button 
+    <motion.button whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}
     onClick={()  => setAddFormToggle(!addFormToggle)}
     className='bg-(--cta) z-2 fixed bottom-8 left-1/2 -translate-x-1/2 w-md h-md rounded-full shadow-md shadow-gray-700 '>
-      <motion.svg animate={{rotate: addFormToggle ? 45 : 0}} className="w-md h-md cursor-pointer" viewBox="0 0 100 100">
+      <motion.svg whileHover={{scale: 1.1}} animate={{rotate: addFormToggle ? 45 : 0}} className="w-md h-md cursor-pointer active:bg-(--cta-dark) active:rounded-full" viewBox="0 0 100 100">
         <line fill="white" stroke="white" strokeWidth="5" id="top" x1="20" y1="50" x2="80" y2="50" />
         <line fill="white" stroke="white" strokeWidth="5" id="middle" x1="50" y1="20" x2="50" y2="80" />
       </motion.svg>
-    </button>
-    <svg className="fixed bottom-0 sm:w-[500px] drop-shadow-xl drop-shadow-gray-600" viewBox="0 0 100 10">
+    </motion.button>
+    <svg  className="fixed bottom-0 sm:w-[500px] drop-shadow-xl drop-shadow-gray-600 " viewBox="0 0 100 10">
       {/* this is for smaller screen size */}
       <path fill="var(--cta)" d="M 0 0 L 42 0 C 45 5 53 8 58 0 L 100 0 L 100 10 L 0 10 Z" />
       {/* this is for larger screen size */}
--- client/app/(HomeComponents)/inputForm.tsx
+++ client/app/(HomeComponents)/inputForm.tsx
@@ -1,5 +1,6 @@
 import { useStore } from "../store"
 import { useSearchParams } from 'next/navigation'
+import { motion } from "motion/react"
 
 export default function InputForm() {
 const {addList, setAddFormToggle}:any = useStore()
@@ -25,13 +26,15 @@ async function formSubmit(formData:any){
   })
 }
     return (
-        <div className="fixed top-0 left-0 w-full h-full bg-black/50 z-1">
-            <form action={formSubmit} className="bg-foreground fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-2 flex flex-col gap-4 justify-center items-center max-w-[500px] w-10/12 h-contain p-sm rounded-2xl">
-                <input type="text" name="title" placeholder="Task" className="w-full h-1/6 p-2 rounded-xl border border-(--cta) text-black" />
-                <input type="date" name="date" placeholder="Date"  className="w-full h-1/6 p-2 rounded-xl border border-(--cta) text-black" />
-                <input type="time" name="startTime" placeholder="startTime"  className="w-full h-1/6 p-2 rounded-xl border border-(--cta) text-black" />
-                <input type="time" name="endTime" placeholder="endTime"  className="w-full h-1/6 p-2 rounded-xl border border-(--cta) text-black" />
-                <button type="submit" className="bg-(--cta) w-full py-2 rounded-xl">Add</button>
+        <div className="fixed bottom-0 left-0 w-full h-full bg-black/50 z-1">
+            <form action={formSubmit} className="bg-foreground fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-2 flex flex-col gap-4 justify-center items-center max-w-[500px] w-10/12 h-contain px-sm py-md rounded-2xl shadow-lg shadow-gray-700">
+                <motion.input type="text" name="title" whileHover={{scale: 1.05}} placeholder="Task" className="w-full h-1/6 p-2 bg-(--accent-orange) border-(--cta) border-b text-(--bg-primary) rounded-md shadow-md shadow-gray-400 focus:bg-foreground focus:outline-none focus:border-(--cta) focus:border-b-2 focus:rounded-none focus:text-black " />
+                <motion.input type="date" onClick={(e) => e.currentTarget.showPicker()} name="date" whileHover={{scale: 1.05}} placeholder="Date"  className="w-full h-1/6 p-2 bg-(--accent-peach) border-(--cta) border-b text-(--bg-primary) rounded-md shadow-md shadow-gray-400 focus:bg-foreground focus:outline-none focus:border-(--cta) focus:border-b-2 focus:rounded-none focus:text-black " />
+                <div className="flex gap-2 w-full">
+                <motion.input type="time" onClick={(e) => e.currentTarget.showPicker()} name="startTime" whileHover={{scale: 1.05}} placeholder="startTime"  className="w-full h-1/6 p-2 bg-(--accent-yellow) border-(--cta) border-b text-(--bg-primary) rounded-md shadow-md shadow-gray-400 focus:bg-foreground focus:outline-none focus:border-(--cta) focus:border-b-2 focus:rounded-none focus:text-black " />       
+                <motion.input type="time" onClick={(e) => e.currentTarget.showPicker()} name="endTime" whileHover={{scale: 1.05}} placeholder="endTime"  className="w-full h-1/6 p-2 bg-(--accent-yellow) border-(--cta) border-b text(--bg-primary) rounded-md shadow-md shadow-gray-400 focus:bg-foreground focus:outline-none focus:border-(--cta) focus:border-b-2 focus:rounded-none focus:text-black " />
+                </div>
+                <motion.button whileHover={{scale: 1.05}} whileTap={{scale: 0.9}} type="submit" className="bg-(--cta) w-full py-2 rounded-xl shadow-md shadow-gray-400 active:bg-(--cta-dark) active:rounded-full">Add</motion.button>
             </form>
         </div>
     );
