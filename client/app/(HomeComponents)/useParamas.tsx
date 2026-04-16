f4f0e6537145fc9037fed5365b9d8198c0f4b84a
Harshit saxena <saxena24harshit@gmail.com>
2026-04-16 19:19:37 +0530

    fix type check

--- client/app/(HomeComponents)/useParamas.tsx
+++ client/app/(HomeComponents)/useParamas.tsx
@@ -3,6 +3,6 @@ import BlogList from "./blogList";
 
 export const dynamic = "force-dynamic";
 
-export default function UseParamas({ searchParams }: { searchParams: { user: string } }) {
+export default function UseParamas({ searchParams }: { searchParams: { user: any } }) {
   return <BlogList user={searchParams.user} />;
 }
